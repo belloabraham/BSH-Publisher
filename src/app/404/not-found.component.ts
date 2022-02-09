83476d644df3fb0ca0eaff48f1d47cbe95bd3cd6
Bello Abraham <belloabraham101@gmail.com>
2022-02-09 14:40:50 +0100

    Added app config with App name

--- src/app/404/not-found.component.ts
+++ src/app/404/not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent{
 
-  constructor() { }
+  constructor(cdRef: ChangeDetectorRef) { 
+      cdRef.detach
+  }
 
 }
