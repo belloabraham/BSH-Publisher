7007146d38b8b8af3f660b568daa42ef58599ceb
Bello Abraham <belloabraham101@gmail.com>
2022-02-09 21:32:01 +0100

    Added auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { Route } from 'src/data/route';
 const routes: Routes = [
   { path: '', redirectTo: Route.auth, pathMatch: 'full' },
   { path: Route.notfound, loadChildren: () => import('./404/not-found.module').then(m => m.NotFoundModule) },
+  { path: Route.auth, loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: '**', redirectTo: Route.notfound },
 ];
 
