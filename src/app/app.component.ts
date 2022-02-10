8af0a7b84c0456761da8b716cc4fb884ee6e4455
Bello Abraham <belloabraham101@gmail.com>
2022-02-10 23:43:04 +0100

    Added no network warning panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { NavigationEnd, NavigationStart, ResolveEnd, ResolveStart, Router } from '@angular/router';
+import { ConnectionService } from 'ng-connection-service';
 import { merge, Observable, of } from 'rxjs';
 import { filter, map, mapTo, tap } from 'rxjs/operators';
 import { Config } from 'src/data/config';
@@ -20,17 +21,22 @@ export class AppComponent implements OnInit, OnDestroy {
   private hideLoaderEvent$!: Observable<boolean>;
   isLoading$!: Observable<boolean>;
   isStarting$: Observable<boolean> = of(true);
+  isNotConnected$: Observable<boolean> = of(true);
 
   constructor(
     private localeService: LocaleService,
-    title:Title,
-    private router: Router) {
-    title.setTitle(Config.appName)
+    title: Title,
+    private connectionService: ConnectionService,
+    private router: Router
+  ) {
+    title.setTitle(Config.appName);
   }
 
   ngOnInit(): void {
-   
-    this.loadLanguageRes()
+    this.loadLanguageRes();
+
+    this.isNotConnected$ = this.connectionService.monitor()
+      .pipe( map(x=>!x))
 
     this.showLoaderEvent$ = this.router.events.pipe(
       filter((e) => e instanceof ResolveStart),
@@ -40,7 +46,7 @@ export class AppComponent implements OnInit, OnDestroy {
     this.hideLoaderEvent$ = this.router.events.pipe(
       filter((e) => e instanceof ResolveEnd),
       mapTo(false),
-      tap(_ => {
+      tap((_) => {
         this.isStarting$ = of(false);
       })
     );
@@ -53,10 +59,10 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   private loadLanguageRes() {
-     this.subscriptions.sink = this.localeService
-       .loadLanguage(Languages.english)
-       .subscribe(() => {
-         this.localeService.setIsLangLoadSuccessfully(true);
-       });
+    this.subscriptions.sink = this.localeService
+      .loadLanguage(Languages.english)
+      .subscribe(() => {
+        this.localeService.setIsLangLoadSuccessfully(true);
+      });
   }
 }
