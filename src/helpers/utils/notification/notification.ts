eeee19fb959a027a81db139d2a45de7ccaa31aed
Bello Abraham <belloabraham101@gmail.com>
2022-02-13 16:15:48 +0100

    Implement Builder Pattern for Notification

---
+++ src/helpers/utils/notification/notification.ts
@@ -0,0 +1,51 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+export class Notification {
+  isClickToClose = true;
+  isBackOverlay = true;
+  isPlainText = true;
+  position?:
+    | 'right-top'
+    | 'right-bottom'
+    | 'left-top'
+    | 'left-bottom'
+    | 'center-top'
+    | 'center-bottom'
+    | 'center-center';
+  timeOut = 3000;
+  messageMaxLenght = 110;
+
+  constructor() {}
+
+  error(
+    msg: string,
+    callback?: () => void,
+    options: Notiflix.INotifyOptions = {
+      position: this.position,
+      plainText: this.isPlainText,
+      backOverlay: this.isBackOverlay,
+      clickToClose: this.isClickToClose,
+      messageMaxLength: this.messageMaxLenght,
+      timeout: this.timeOut,
+      cssAnimationStyle: 'from-top',
+    }
+  ) {
+    Notify.failure(msg, callback, options);
+  }
+
+  success(
+    msg: string,
+    callback?: () => void,
+    options: Notiflix.INotifyOptions = {
+      position: this.position,
+      plainText: this.isPlainText,
+      backOverlay: this.isBackOverlay,
+      clickToClose: this.isClickToClose,
+      messageMaxLength: this.messageMaxLenght,
+      timeout: this.timeOut,
+      cssAnimationStyle: 'from-top',
+    }
+  ) {
+    Notify.success(msg, callback, options);
+  }
+}
