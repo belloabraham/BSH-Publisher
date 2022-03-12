c2089a73fecbb9304f54f35038e6a0676607f66e
Bello Abraham <belloabraham101@gmail.com>
2022-03-12 22:27:32 +0100

    Updated details and auth comp

--- src/app/welcome/dashboard/payment/details/details.component.ts
+++ src/app/welcome/dashboard/payment/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
+  addPaymentDetailsClick = false
+  paymentDeatilsForm!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log('')
+  }
+
+  addPaymentDetails() {
+    this.addPaymentDetailsClick = true
   }
 
 }
