540623bd3767bdd99d384b65553ae38dfdf27b21
Bello Abraham <belloabraham101@gmail.com>
2022-03-16 20:31:13 +0100

    Added custom scrollbars

--- src/app/welcome/publish-your-book/publish-your-book.module.ts
+++ src/app/welcome/publish-your-book/publish-your-book.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { PublishYourBookRoutingModule } from './publish-your-book-routing.module';
 import { PublishYourBookComponent } from './publish-your-book.component';
+import { LyExpansionModule } from '@alyle/ui/expansion';
+import { LyButtonModule } from '@alyle/ui/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslocoModule } from '@ngneat/transloco';
 
 
 @NgModule({
@@ -11,7 +15,11 @@ import { PublishYourBookComponent } from './publish-your-book.component';
   ],
   imports: [
     CommonModule,
-    PublishYourBookRoutingModule
+    PublishYourBookRoutingModule,
+    LyExpansionModule,
+    LyButtonModule,
+    ReactiveFormsModule,
+    TranslocoModule
   ]
 })
 export class PublishYourBookModule { }
