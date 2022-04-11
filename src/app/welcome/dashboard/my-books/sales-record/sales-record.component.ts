9b9cd42ed7ebf58fbf8f59706e6f982664d6c91e
Bello Abraham <belloabraham101@gmail.com>
2022-04-11 13:54:14 +0100

    Added GridJs Module

--- src/app/welcome/dashboard/my-books/sales-record/sales-record.component.ts
+++ src/app/welcome/dashboard/my-books/sales-record/sales-record.component.ts
@@ -10,6 +10,7 @@ export class SalesRecordComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log()
   }
 
 }
--- src/app/welcome/dashboard/my-books/sales-record/sales-record.module.ts
+++ src/app/welcome/dashboard/my-books/sales-record/sales-record.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SalesRecordRoutingModule } from './sales-record-routing.module';
 import { SalesRecordComponent } from './sales-record.component';
-
+import { GridJsAngularModule } from 'gridjs-angular';
 
 @NgModule({
-  declarations: [
-    SalesRecordComponent
-  ],
-  imports: [
-    CommonModule,
-    SalesRecordRoutingModule
-  ]
+  declarations: [SalesRecordComponent],
+  imports: [CommonModule, SalesRecordRoutingModule, GridJsAngularModule],
 })
-export class SalesRecordModule { }
+export class SalesRecordModule {}
