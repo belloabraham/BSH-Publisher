b0fa36da265913b3c17c2b4480927157f0bb768e
Bello Abraham <belloabraham101@gmail.com>
2022-04-12 16:57:59 +0100

    Updated sales record component

--- src/app/welcome/dashboard/my-books/sales-record/sales-record.component.ts
+++ src/app/welcome/dashboard/my-books/sales-record/sales-record.component.ts
@@ -1,16 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { UserConfig } from 'gridjs';
+import { Config } from 'src/data/config';
+import { Display } from 'src/helpers/utils/display';
 
 @Component({
   selector: 'app-sales-record',
   templateUrl: './sales-record.component.html',
-  styleUrls: ['./sales-record.component.scss']
+  styleUrls: ['./sales-record.component.scss'],
 })
 export class SalesRecordComponent implements OnInit {
+  public gridConfig: UserConfig = {
+    language: {
+      search: {
+        placeholder: '🔍 Search...',
+      },
+    },
+    columns: ['Name', 'Email', 'Phone Number'],
+    pagination: {
+      enabled:true,
+      limit: 50,
+    },
+    sort:true,
+    search: true,
+    data: [
+      ['John', 'john@example.com', '(353) 01 222 3333'],
+      ['Mark', 'mark@gmail.com', '(01) 22 888 4444'],
+      ['Eoin', 'eoin@gmail.com', '0097 22 654 00033'],
+      ['Sarah', 'sarahcdd@gmail.com', '+322 876 1233'],
+      ['Afshin', 'afshin@mail.com', '(353) 22 87 8356'],
+    ],
+    className: {
+      table: 'table table-striped table-hover table-bordered',
+    },
+    style: {
+      table: {
+        'font-size': Display.remToPixel(1.2),
+      },
+      td: {
+        'font-family': Config.defaultFontFamily,
+      },
+    },
+  };
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    console.log()
+    console.log();
   }
-
 }
