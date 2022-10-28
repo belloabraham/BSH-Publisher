addfbc01a55b03df8b3cd048d46a3aed78c4f464
Bello Abraham <belloabraham101@gmail.com>
2022-10-28 02:01:28 -0500

    Updated dependecies

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,7 @@ export const environment = {
   firebase: {
     projectId: 'bookshelfhub-fa4cf',
     appId: '1:665623853292:web:1732522f5d8fafff368dc9',
-    databaseURL:
-      'https://bookshelfhub-fa4cf-default-rtdb.europe-west1.firebasedatabase.app',
+    databaseURL: 'https://bookshelfhub-fa4cf-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'bookshelfhub-fa4cf.appspot.com',
     locationId: 'europe-west',
     apiKey: 'AIzaSyB_xY6c3EBAiLbuBL_pYs3KqSC38YRPhmw',
